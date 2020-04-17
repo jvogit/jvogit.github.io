375ec9bf6be1968764b8decfe7d07252535e7cf3
jvogit <justinxvopro@gmail.com>
2020-04-17 00:08:49 -0700

    Updates

---
+++ precache-manifest.ca555a4aa3259d56ed9a01fd6d8df842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9387fbb947fdb0d7b208422cdb671ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2864826c15a06b6af8e1",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4d4ccfd0617543447cee",
+    "url": "/static/js/2.05d573f5.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/static/js/2.05d573f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2864826c15a06b6af8e1",
+    "url": "/static/js/main.688292f7.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
